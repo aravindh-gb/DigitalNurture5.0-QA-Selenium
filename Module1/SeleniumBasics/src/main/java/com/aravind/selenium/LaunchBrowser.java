c29714ad76b02d2820c0398de2738718f9274d64
aravindh-gb <166371665+aravindh-gb@users.noreply.github.com>
2026-07-16 20:38:14 +0530

    Added Selenium Maven project

---
+++ Module1/SeleniumBasics/src/main/java/com/aravind/selenium/LaunchBrowser.java
@@ -0,0 +1,22 @@
+package com.aravind.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LaunchBrowser {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+
+        System.out.println("Title: " + driver.getTitle());
+
+        driver.quit();
+    }
+}
